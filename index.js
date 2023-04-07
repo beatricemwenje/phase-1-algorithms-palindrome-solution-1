811014324b25f07322f427a2bd53a455858ebd4d
beatrice <beatrice.mwenje@student.moringaschool>
2023-04-07 22:50:53 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  function reverseString(word) {
+    return word.split("").reverse().join("")
+  }
+  const reversedWord = reverseString(word);
+  return word === reversedWord
+
 }
 
 /* 
   Add your pseudocode here
+/*
+Add your pseudocode here
+reverse the input string
+Compare the reversed string if its the same as the input
+  return true if  they do
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+  Created a function that took the word and split it which returns an array that can be passed the reverse() method then joined it using the join() function.
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +36,17 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+  // You can run `node index.js` to view these console logs
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+
+
